3a786a13433d1e8be231998eb5407c8b20df5226
PedroHenriqueFonsecaMelo <83472390+PedroHenriqueFonsecaMelo@users.noreply.github.com>
2025-11-07 21:18:26 -0300

    F.SI/1

--- src/main/java/umc/exs/model/daos/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package umc.exs.model.daos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import umc.exs.model.entidades.foundation.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    
-}
