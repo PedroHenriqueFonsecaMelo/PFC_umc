784276e4b087d85f938e824eaa1a7bd27ec2c5d0
PedroHenriqueFonsecaMelo <83472390+PedroHenriqueFonsecaMelo@users.noreply.github.com>
2026-02-26 22:11:10 -0300

    1.8.3.1

--- src/main/java/umc/exs/controller/LivroViewController.java
+++ src/main/java/umc/exs/controller/LivroViewController.java
@@ -11,12 +11,12 @@ public class LivroViewController {
     // Rota para abrir o formulário de cadastro/venda
     @GetMapping("/vender")
     public String paginaVender() {
-        return "venda_livro"; // Procura por venda_livro.html em /templates
+        return "produto/venda_livro";
     }
 
     // Rota para abrir a vitrine de compras
     @GetMapping("/vitrine")
     public String paginaVitrine() {
-        return "vitrine_livros"; // Procura por vitrine_livros.html em /templates
+        return "produto/vitrine_livros"; 
     }
 }
